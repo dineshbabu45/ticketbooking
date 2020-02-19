940ba05daa35bab875c424e052c4eac2b4eeb54c
dineshbabu45 <dinesh45babu@gmail.com>
2020-02-19 22:49:22 +0530

    Login and SignUp

---
+++ Account.Entity/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Account.Entity
+{
+    public class Account
+    {
+        public String EmailId { get; set; }
+        public String Password { get; set; }
+        public String Name { get; set; }
+        public byte Age { get; set; }
+        public String Gender { get; set; }
+
+    }
+}
